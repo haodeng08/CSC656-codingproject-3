5d152ea77d1ddb62cc81811f0d8792c047d94aeb
haodeng08 <haodeng08@gmail.com>
2025-04-29 17:51:48 -0700

    update openmp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -13,9 +13,8 @@ const char* dgemv_desc = "OpenMP dgemv.";
  */
 
 void my_dgemv(int n, double* A, double* x, double* y) {
-
-   #pragma omp parallel for
    /*
+   #pragma omp parallel 
    {
       int nthreads = omp_get_num_threads();
       int thread_id = omp_get_thread_num();
@@ -26,12 +25,20 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
+   
+    #pragma omp parallel for
+    // For each row in matrix A
     for (int i = 0; i < n; i++) {
-        // Each thread will work on different rows of A
+        // Calculate row offset once per row
+        int row_offset = i * n;
+        double row_sum = 0.0;
+        // For each column in matrix A
         for (int j = 0; j < n; j++) {
-            y[i] += A[i*n + j] * x[j];
+            // Use pre-calculated row offset
+            row_sum += A[row_offset + j] * x[j];
         }
+        // Add result to existing y value
+        y[i] += row_sum;
     }
-
 }
 
